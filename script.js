f47de6968b73192f1d0c4cb3281bb89177b29608
gabrielagentile <88155095+gabrielagentile@users.noreply.github.com>
2022-01-30 13:53:21 -0300

    conserto js 2

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+search()
 function search(){
      var data = document.getElementById ("date").value
     $.ajax({url:`https://api.nasa.gov/planetary/apod?api_key=Gu0MZYR5dc4VHP5VISzp9eQINX6AibiJsP8BR9Ai&date=${data}`,
     
-        success: function(parametro) {
-        $('#img').attr('src', parametro.url)
-        $('#title').text( parametro.title)
-        $('#explanation').text( parametro.explanation)
-        $('#copyright').text( parametro.copyright);}})
+        success: function(data) {
+        $('#img').attr('src', data.url)
+        $('#title').text(data.title)
+        $('#explanation').text(data.explanation)
+        $('#copyright').text(data.copyright);}})
     }
