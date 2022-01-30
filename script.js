288ac3d02093e86508ab36b32177943840c11caf
gabrielagentile <88155095+gabrielagentile@users.noreply.github.com>
2022-01-30 13:47:19 -0300

    conserto js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-$("#submit").click(function(event){
-    event.preventDefault();
-    pesquisar();
-
-function pesquisar(){
+function search(){
      var data = document.getElementById ("date").value
     $.ajax({url:`https://api.nasa.gov/planetary/apod?api_key=Gu0MZYR5dc4VHP5VISzp9eQINX6AibiJsP8BR9Ai&date=${data}`,
     
